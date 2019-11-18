6860f14b159113f7ff2ee517d6fe2ac01fc17e51
947418354 <wy63wy63wy63@163.com>
2019-11-18 19:42:41 +0800

    a

--- my-utils.js
+++ my-utils.js
@@ -1,3 +1,13 @@
+/**
+ * 得到url中指定的参数值
+ * @param {参数名 String} name 
+ */
+export function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var reg = new RegExp("[\\?&]" + name + "=([^&#\/]*)"), results = reg.exec(location.search);
+  return results == null ? "" : decodeURIComponent(results[1]);
+}
+
 // 节流函数
 export function throttle(fn, delay, atleast) {
   /** 函数节流方法: fn 延时调用函数; delay 延迟多长时间; atleast 至少多长时间触发一次; return function 延迟执行的方法;***/
