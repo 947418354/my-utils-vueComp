249c90638c16fb0c1e1c50d017771ba94299e6f5
szwb209 <szwb209@szwb.fsc.cntaiping.com>
2020-05-19 16:39:00 +0800

    a

--- src/utils/date/index.js
+++ src/utils/date/index.js
@@ -19,6 +19,40 @@ const isLeapYear = (currentFullYear) => {
 }
 
 /**
+ * 是否为闰年
+ * @param {number} currentDay 
+ */
+export const isLeapYear = (currentFullYear) => {
+  let isLeapYear =  (currentFullYear % 100 !== 0 && currentFullYear % 4 === 0) 
+                    || (currentFullYear % 400 === 0 && currentFullYear % 3200 !== 0) 
+                    || (currentFullYear % 172800 === 0)
+  return isLeapYear
+}
+
+ /**
+  * 自然日期加法
+  * @param {*} currentDay 
+  * @param {*} n 
+  */
+export const getNextNNatureYear = (currentDay, n) => {
+  let splitDate = currentDay.split('-')
+  let year = splitDate[0]
+  let month = splitDate[1]
+  let day = splitDate[2]
+  
+  let targetYear = Number(year) + n
+  if (isLeapYear(Number(year)) && month == 2 && day == 29) {
+    if (isLeapYear(Number(targetYear))) {
+      return [targetYear, month, day].join('-')
+    } else {
+      return [targetYear, month, Number(day) - 1].join('-')
+    }
+  } else {
+    return [targetYear, month, day].join('-')
+  }
+}
+
+/**
  * 得到格式化后的日期字符串
  * @param date 任何支持 new Date(date) 的参数,包括日期对象 时间戳 日期字符串
  * @param join 日期拼接符
--- src/utils/str/index.js
+++ src/utils/str/index.js
@@ -1,5 +1,5 @@
 /**
- * 字符串模板替换.
+ * 字符串模板替换
  * @param {被处理的字符串} str "你好$name，欢迎到$home。这是你第$count次来$home"
  * @param {提供数据的对象} obj {
       name: 'jack',
@@ -58,4 +58,26 @@ export function strSplitByWords(str, words) {
     }
   }
   return output
+}
+
+/**
+ * 返回一个数字字母组成的随机串
+ * @param {随机长度起值} minL 
+ * @param {随机长度终值} maxL 
+ */
+export function getRandomStr(minL, maxL) {
+  if(!maxL) maxL = minL
+  var str = "",
+    range = minL, //min
+    arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+      'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+      'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ];
+  // 随机产生
+  range = Math.round(Math.random() * (maxL - minL)) + minL;
+  for (var i = 0; i < range; i++) {
+    var pos = Math.round(Math.random() * (arr.length - 1));
+    str += arr[pos];
+  }
+  return str;
 }
