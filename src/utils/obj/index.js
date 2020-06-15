9d8fad521ffee4a53464017c68d42685deaed6da
szwb209 <szwb209@szwb.fsc.cntaiping.com>
2020-06-15 15:27:04 +0800

    拷贝

--- src/utils/obj/index.js
+++ src/utils/obj/index.js
@@ -1,3 +1,31 @@
+//浅拷贝函数
+function shallowCopy(target){
+	var obj;
+	var str = Object.prototype.toString.call(target);
+	if(str == '[object Array]'){
+		obj = [];
+		for (var i = 0; i < target.length; i++) {
+			obj.push(target[i]);
+		}
+	}else if(str == '[object Object]'){
+		obj = {};
+		for(var i in target){
+			obj[i] = target[i];
+		}
+	}else{
+		return target;
+	}
+	return obj;
+}
+
+//深拷贝函数
+function deepCopy (obj) {
+	if (obj === null || typeof obj !== 'object') return obj;
+	var cpObj = obj instanceof Array ? [] : {};
+	for (var key in obj) cpObj[key] = deepCopy(obj[key]);
+	return cpObj;
+}
+
 // 是否为空对象
 export function isEmptyObject(o) {
   var tmp
@@ -15,4 +43,14 @@ export function checkObjectValues(data) {
     return !hasEmptyValue;
   }
   return true;
+}
+
+export {
+  shallowCopy,
+  deepCopy
+}
+
+export default {
+  shallowCopy,
+  deepCopy
 }
