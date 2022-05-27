d9f74e301e7ca166bd86b1e87c775222b7afe343
zouhui <wy63wy63wy63@163.com>
2022-05-27 14:46:23 +0800

    feat: 增加tabs组件

---
+++ src/components/menuC.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="compMenu">
+      <ul class="compMenu-ul">
+          <li class="compMenu-li" :class="{active: active === item.value}" v-for="(item, i) of list" :key="item.valur">{{item.label}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        list: {
+            type: Array,
+            default() {return []}
+        },
+        active: [String, Number]
+    }
+}
+</script>
+
+<style>
+.compMenu {
+
+}
+.compMenu-ul {
+    display: flex;
+    font-size: 16px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: rgba(255, 255, 255, 0.65);
+    line-height: 22px;
+    letter-spacing: 1px;
+    list-style: none;
+    margin: 0;
+}
+.compMenu-li {
+    position: relative;
+    padding: 37px 40px;
+    flex-shrink: 0;
+}
+.compMenu-li:hover{
+    color: #FFFFFF;
+}
+.compMenu-li.active {
+    font-weight: 600;
+    color: #FFFFFF;
+}
+.compMenu-li.active::after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background: #FFFFFF;
+    left: 0;
+    bottom: 0;
+}
+</style>
---
+++ src/components/tabs.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="compTabs">
+    <ul class="compTabs-ul">
+      <li
+        class="compTabs-li"
+        :class="{ active: value === item.value }"
+        v-for="(item, i) of list"
+        :key="item.valur"
+        @click="handleClickTabsItem(item.value)"
+      >
+        {{ item.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    value: [String, Number],
+  },
+  methods: {
+    handleClickTabsItem(value) {
+      this.$emit("update:value", value);
+    },
+  },
+};
+</script>
+
+<style>
+.compTabs {
+}
+.compTabs-ul {
+  display: flex;
+  font-size: 16px;
+  font-family: PingFangSC-Medium, PingFang SC;
+  font-weight: 500;
+  color: rgba(0, 0, 0, 0.85);
+  list-style: none;
+  margin: 0;
+}
+.compTabs-li {
+  position: relative;
+  width: 366px;
+  text-align: center;
+  line-height: 80px;
+}
+.compTabs-li:hover {
+  color: #ffffff;
+}
+.compTabs-li.active {
+  font-weight: 600;
+  color: #ffffff;
+}
+.compTabs-li.active::after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  background: #ffffff;
+  left: 0;
+  bottom: 0;
+}
+</style>
